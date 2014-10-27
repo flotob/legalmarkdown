d220d7e95fdbfd32e76cd7720ab02ce2cf77b9ba
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-10-27 16:43:56 -0400

    update docs in reader

--- lmd/reader.go
+++ lmd/reader.go
@@ -9,11 +9,11 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// ReadAFile is a convenience function. given a filename string, reads the file and passes it back to
-// the calling function as a string.
+// ReadAFile is a convenience function. Given a filename string, reads the file and passes it back to
+// the calling function as a string. Given a "-" switch the function will read from stdin rather than
+// from a file.
 func ReadAFile(file_to_read string) string {
 
-    // if "-" is passed as the file to read, read in from stdin instead.
     if file_to_read == " -" || file_to_read == "-" {
 
         std_in_read, std_in_err := ioutil.ReadAll(os.Stdin)
@@ -33,12 +33,11 @@ func ReadAFile(file_to_read string) string {
 	return contents
 }
 
-// ImportIncludedFiles. included files need to be imported into the primary contents string. this function manages
-// that process. first it compiles a regular expression which will search for the trigger string
-//   @include PARTIAL
+// ImportIncludedFiles handles importing files into the primary contents string. First it compiles a
+// regular expression which will search for the trigger string `@include PARTIAL`.
 //
-// if one or more match is found, the function will simply replace the `@include PARTIAL` line with the
-// read in string of the included partial. the complete string will be returned to the calling function.
+// If one or more match is found, the function will simply replace the `@include PARTIAL` line with the
+// read in string of the included partial. The complete string will be returned to the calling function.
 func ImportIncludedFiles(fileContents string) string {
 	importRegExp := regexp.MustCompile(`(?m)^@include (.*?)$`)
 
@@ -53,9 +52,9 @@ func ImportIncludedFiles(fileContents string) string {
 	}
 }
 
-// ParseTemplateToFindParameters. paramaters may be passed to the parser either separately from the
-// template file or as part of the template file. this function manages the process of stripping paramaters
-// out of a template file. the function first compiles a YAML Front Matter regular expression. then if a
+// ParseTemplateToFindParameters handles paramaters which are passed to the parser either separately from the
+// template file or as part of the template file. This function manages the process of stripping paramaters
+// out of a template file. The function first compiles a YAML Front Matter regular expression. Then if a
 // match for that regular expression is found, the contents of the template file are replaced with an empty
 // string and the YAML front matter is returned, along with the replaced contents (both as strings) to the
 // calling function.
@@ -71,9 +70,9 @@ func ParseTemplateToFindParameters(fileContents string) (string, string) {
 	}
 }
 
-// UnmarshallParameters. yaml and json paramaters must be unmarshalled into the paramaters map. this function
-// is responsible for unmarshalling the paramaters from yaml or json strings into (first a byte array) and
-// subsequently into the paramaters map which is returned to the calling function.
+// UnmarshallParameters unmarshalls paramaters either in yaml (TBD) or json into the paramaters map. This
+// function is responsible for unmarshalling the paramaters from yaml or json strings into (first a byte
+// array) and subsequently into the paramaters map which is returned to the calling function.
 func UnmarshallParameters(parameters string) map[string]string {
 	// TODO: make this smarter... should be able to also parse JSON if the YAML unmarshall fails
 	parameter_bytes := []byte(parameters)
@@ -82,8 +81,8 @@ func UnmarshallParameters(parameters string) map[string]string {
 	return param
 }
 
-// MergeParameters. this is a convenience function which will merge two hash maps. any conflicting parameters
-// in the two maps will be resolved in favor of the *first* map which is passed. that is to say that the first
+// MergeParameters is a convenience function which will merge two hash maps into one. Any conflicting parameters
+// in the two maps will be resolved in favor of the *first* map which is passed. That is to say that the first
 // map passed to the function, the `superior_map` map, will overwrite the `sublimated_map`.
 func MergeParameters(superior_map map[string]string, sublimated_map map[string]string) map[string]string {
 	for k, v := range superior_map {
