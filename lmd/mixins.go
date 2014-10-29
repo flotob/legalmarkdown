a935a5bc85a12eed074f065d7d88605822825067
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-10-28 22:06:20 -0400

    document mixins

--- lmd/mixins.go
+++ lmd/mixins.go
@@ -6,20 +6,45 @@ import (
 	"strings"
 )
 
-// HandleMixins .... does shit.
+// HandleMixins is the primary handling function for the mixin and optional clause parsing exercise
+// which is a subset of the overall parse job.
+//
+// The first thing which the function will do is to extract the parameters which we know are applicable
+// to the structured_headers portion of the overall parse job rather than the mixins portion of the
+// overall parse job. These parameters are parked into a new map during the remainder of the mixin
+// operation.
+//
+// After the parameters we do not want to handle have been parked, the function then runs through the
+// optional clauses parse adding those optional clauses which have been turned on, and taking out of
+// the final document those optional clauses which have been turned off.
+//
+// When the optional clauses have finalized, then the function runs through the simple text mixins
+// and replaces all of the keys established in the text of the template with the values established
+// in the parameters.
+//
+// Finally a simple cleanup function is called to compress extraneous white space and then the function
+// returns the parsed and corrected contents along with the parked parameters which are relevant to
+// the structured_headers phase of the overall parse.
 func HandleMixins(contents string, parameters map[string]string) (string, map[string]string) {
-	// pull out parameters we *know* we don't want to mess with.
+
+    // create a parking_lot variable and park the parameters we know we don't want to mess with
+    // during this phase
 	var params_parking_lot map[string]string
-	parameters, params_parking_lot = prepare_params_parking_lot(parameters)
-	contents, parameters = run_optional_clauses(contents, parameters)
-	contents, parameters = run_text_mixins(contents, parameters)
-	contents = clean_up_post_mixins(contents)
-	parameters = params_parking_lot
-	return contents, parameters
+	parameters, params_parking_lot = prepareParamsParkingLot(parameters)
+
+    // run the optional clauses and mixins
+	contents, parameters = runOptionalClauses(contents, parameters)
+	contents = runTextMixins(contents, parameters)
+
+    // perform some simple clean up
+	contents = cleanUpPostMixins(contents)
+
+	return contents, params_parking_lot
 }
 
-// prepare_params_parking_lot pulls out parameters we *know* we don't want to mess with.
-func prepare_params_parking_lot(parameters map[string]string) (map[string]string, map[string]string) {
+// prepareParamsParkingLot pulls out parameters we *know* we don't want to mess with during the mixin
+// phase of the overall parse.
+func prepareParamsParkingLot(parameters map[string]string) (map[string]string, map[string]string) {
 
 	// define the parameters we want to blacklist in a slice of strings
 	parameters_blacklist_strings := []string{"level-[0-9]", "no-reset", "no-indent", "level-style"}
@@ -47,35 +72,60 @@ func prepare_params_parking_lot(parameters map[string]string) (map[string]string
 
 }
 
-// run_optional_clauses ....
-func run_optional_clauses(contents string, parameters map[string]string) (string, map[string]string) {
-	clauses_added, clauses_deleted := separate_optional_clauses(parameters)
+// runOptionalClauses is the guardian of the circuit which needs to be run to ensure that all of the
+// optional clauses (nested and single) have been properly handled. it first sends the current parameters
+// map to the separateOptionalClauses function to get those which need to be added and those which need
+// to be deleted. after that it goes into an infinite loop which runs through the runThisOptionalClause
+// pattern for both the to_add and to_delete slices until both have been completely exhausted (which occurs
+// after all the "true" and "false" have been expunged from the parameters map).
+func runOptionalClauses(contents string, parameters map[string]string) (string, map[string]string) {
+
+	clauses_added, clauses_deleted := separateOptionalClauses(parameters)
+
 	for {
-		contents, clauses_added = run_this_optional_clause(contents, clauses_added, true, parameters)
-		contents, clauses_deleted = run_this_optional_clause(contents, clauses_deleted, false, parameters)
-		clauses_added, clauses_deleted = separate_optional_clauses(parameters)
+
+        // first pass
+		contents, clauses_added = runThisOptionalClause(contents, clauses_added, true, parameters)
+		contents, clauses_deleted = runThisOptionalClause(contents, clauses_deleted, false, parameters)
+
+        // reload the slices against the parameters
+		clauses_added, clauses_deleted = separateOptionalClauses(parameters)
+
+        // check if the reloaded slices are of 0 and 0 length, break if yes
 		if len(clauses_added) == 0 && len(clauses_deleted) == 0 {
 			break
 		}
+
 	}
+
 	return contents, parameters
 }
 
-func run_text_mixins(contents string, parameters map[string]string) (string, map[string]string) {
+// runTextMixins is a very simple function. it loops through the remaining parameters (it is called
+// after the optional clauses have run) and replaces the keys with the values from the parameters
+// map which remains.
+func runTextMixins(contents string, parameters map[string]string) (string) {
+
 	for to_replace, replacer := range parameters {
 		mixin_pattern := regexp.MustCompile(fmt.Sprintf(`(\{\{%v\}\})`, to_replace))
 		if mixin_pattern.MatchString(contents) {
 			contents = mixin_pattern.ReplaceAllString(contents, replacer)
 		}
 	}
-	return contents, parameters
+
+	return contents
 }
 
-func clean_up_post_mixins(contents string) string {
+// cleanUpPostMixins is a simple function which compresses excessive whitespace.
+func cleanUpPostMixins(contents string) string {
+
+    // when there are more than two new_lines, squeeze those into two.
 	too_many_lines := regexp.MustCompile(`\n\n+`)
 	if too_many_lines.MatchString(contents) {
 		contents = too_many_lines.ReplaceAllString(contents, "\n\n")
 	}
+
+    // when there are more than two spaces, squeeze those into one.
 	too_many_space := regexp.MustCompile(` {2,}`)
 	if too_many_space.MatchString(contents) {
 		contents = too_many_space.ReplaceAllString(contents, " ")
@@ -84,9 +134,15 @@ func clean_up_post_mixins(contents string) string {
 	return contents
 }
 
-func separate_optional_clauses(parameters map[string]string) ([]string, []string) {
+// separateOptionalClauses checks the parameters map and pulls out those parameters with a
+// key:value of _:true and adds those values to the clauses_to_keep slice. those parameters
+// with a key:value of _:false are added to the clauses_to_delete slice. both slices are then
+// returned to the calling function.
+func separateOptionalClauses(parameters map[string]string) ([]string, []string) {
+
 	clauses_to_delete := []string{}
 	clauses_to_keep := []string{}
+
 	for clause, status := range parameters {
 		if status == "true" {
 			clauses_to_keep = append(clauses_to_keep, clause)
@@ -94,52 +150,71 @@ func separate_optional_clauses(parameters map[string]string) ([]string, []string
 			clauses_to_delete = append(clauses_to_delete, clause)
 		}
 	}
+
 	return clauses_to_keep, clauses_to_delete
 }
 
-func run_this_optional_clause(contents string, clauses []string, add_or_delete bool, parameters map[string]string) (string, []string) {
+// runThisOptionalClause is the primary optional clause parsing function. it parses the contents against
+// a set of optional clauses, running through the slice of strings it is given in the clauses slice one
+// time. in general this function will be called a minimum of twice if there are optional clauses in the
+// template file -- once for those optional clauses turned on and once for optional clauses turned off.
+func runThisOptionalClause(contents string, clauses []string, add_or_delete bool, parameters map[string]string) (string, []string) {
+
 	for _, clause := range clauses {
-		sub_clause, _ := "", ""
-		pri_pattern := regexp.MustCompile(fmt.Sprintf(`(?m)(\[\{\{%v\}\}\s*?)(.*?\n*?)(\])`, clause))
-		sub_pattern := regexp.MustCompile(`(?m)\[\{\{(\S+?)\}\}\s*?`)
+
+        // there are two relevant regex's one for primary optional clauses and one to check if there are
+        // nested optional clauses
+        pri_pattern := regexp.MustCompile(fmt.Sprintf(`(?m)\[\{\{%v\}\}\s*?(.*?\n*?)\]`, clause))
+        sub_pattern := regexp.MustCompile(`(?m)\[\{\{(\S+?)\}\}\s*?`)
+
+        // first we check if there is a match within the overall content. are there any optional clauses
+        // at all? if there are then we dump the found group from the regex into the sub_clause string.
+		var sub_clause string
 		if pri_pattern.MatchString(contents) {
 			da_finding := pri_pattern.FindAllStringSubmatch(contents, -1)
-			sub_clause = da_finding[0][2]
+			sub_clause = da_finding[0][1]
 		}
+
+        // if the sub_clause (what is between the square brackets) has another nested optional clause
+        // then the pattern will break because we do not want to do anything until we have found the
+        // inner-most nested optional clause which will make our non-greedy regex work.
 		if sub_pattern.MatchString(sub_clause) {
 			continue
 		}
+
+        // the add_or_delete variable is a boolean if it is an add then the subclause will replace the
+        // overall found pattern, if it is false then the whole thing will be replaced with an empty
+        // string.
 		if add_or_delete {
 			contents = pri_pattern.ReplaceAllString(contents, strings.TrimSpace(sub_clause))
 		} else {
 			contents = pri_pattern.ReplaceAllString(contents, "")
 		}
+
+        // the final step is to delete the matched clause from the parameters if it is not found in the
+        // contents any longer. this is because we will refresh the loop overall in the runOptionalClauses
+        // function so we don't want to mess with this key any longer.
 		if !pri_pattern.MatchString(contents) {
-			clauses = removeFromSlice(clauses, clause)
 			delete(parameters, clause)
 		}
 	}
+
 	return contents, clauses
 }
 
-// back_out_of_parking_lot adds the parked parameters back into the full parameters list before those
-// are sent back to the primary parser.
-// func back_out_of_parking_lot(parameters map[string]string, params_parking_lot map[string]string) map[string]string {
-//     for key, val := range params_parking_lot {
-//         parameters[key] = val
-//     }
-//     return parameters
-// }
-
-func includedInThisSlice(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
+// includedInThisSlice is a convenience function which simply checks if a particular string is included
+// within a slice of strings.
+func includedInThisSlice(slice []string, element string) bool {
+	for _, ele := range slice {
+		if ele == element {
 			return true
 		}
 	}
 	return false
 }
 
+// removeFromSlice removes one string from a slice of strings and returns the redone slice of strings
+// to the calling method.
 func removeFromSlice(remove_from_me []string, ele_to_remove string) []string {
 	var slice_with_ele_removed []string
 	for i, ele := range remove_from_me {
@@ -150,12 +225,14 @@ func removeFromSlice(remove_from_me []string, ele_to_remove string) []string {
 	return slice_with_ele_removed
 }
 
-func areTheseSlicesEqual(a, b []string) bool {
-	if len(a) != len(b) {
+// areTheseSlicesEqual checks if two slices full of string are equal in that they have the same length
+// and each of their elements are equal.
+func areTheseSlicesEqual(first_slice, second_slice []string) bool {
+	if len(first_slice) != len(second_slice) {
 		return false
 	}
-	for i := range a {
-		if a[i] != b[i] {
+	for i := range first_slice {
+		if first_slice[i] != second_slice[i] {
 			return false
 		}
 	}
