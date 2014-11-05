38c0f69ee2fd669993f9cfabb879e7d0aa76184f
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-11-04 22:33:32 -0500

    go fmt params maker

--- lmd/make_params.go
+++ lmd/make_params.go
@@ -1,149 +1,155 @@
 package lmd
 
 import (
-    "gopkg.in/yaml.v2"
-    "regexp"
-    "strconv"
+	"gopkg.in/yaml.v2"
+	"regexp"
+	"strconv"
 )
 
+// HandleParameterAssembly ...
 func HandleParameterAssembly(contents string, parameters map[string]string) string {
 
-    if len(parameters) == 0 {
-        return contents
-    }
+	if len(parameters) == 0 {
+		return contents
+	}
 
-    mixins := make(map[string]string)
-    optClauses := make(map[string]string)
-    headers := make(map[string]string)
-    styles := make(map[string]string)
+	mixins := make(map[string]string)
+	optClauses := make(map[string]string)
+	headers := make(map[string]string)
+	styles := make(map[string]string)
 
-    contents, mixins = findTheMixins(contents, parameters)
-    contents, optClauses = findTheOptClauses(contents, parameters)
-    contents, headers, styles = findTheLeaders(contents, parameters)
-    contents = reAssembleTheFile(contents, mixins, optClauses, headers, styles)
+	contents, mixins = findTheMixins(contents, parameters)
+	contents, optClauses = findTheOptClauses(contents, parameters)
+	contents, headers, styles = findTheLeaders(contents, parameters)
+	contents = reAssembleTheFile(contents, mixins, optClauses, headers, styles)
 
-    return contents
+	return contents
 
 }
 
+// findTheMixins ...
 func findTheMixins(contents string, parameters map[string]string) (string, map[string]string) {
 
-    mixins := make(map[string]string)
-    mixinPattern := regexp.MustCompile(`[^\[]{{(\S+)}}`)
+	mixins := make(map[string]string)
+	mixinPattern := regexp.MustCompile(`[^\[]{{(\S+)}}`)
 
-    if mixinPattern.MatchString(contents) {
-        for _, matchSlice := range mixinPattern.FindAllStringSubmatch(contents, -1) {
-            if _, exists := parameters[matchSlice[1]]; !exists {
-                mixins[matchSlice[1]] = ""
-            } else {
-                mixins[matchSlice[1]] = parameters[matchSlice[1]]
-            }
-        }
-    }
+	if mixinPattern.MatchString(contents) {
+		for _, matchSlice := range mixinPattern.FindAllStringSubmatch(contents, -1) {
+			if _, exists := parameters[matchSlice[1]]; !exists {
+				mixins[matchSlice[1]] = ""
+			} else {
+				mixins[matchSlice[1]] = parameters[matchSlice[1]]
+			}
+		}
+	}
 
-    return contents, mixins
+	return contents, mixins
 }
 
+// findTheOptClauses ...
 func findTheOptClauses(contents string, parameters map[string]string) (string, map[string]string) {
 
-    optClauses := make(map[string]string)
-    optClausesPattern := regexp.MustCompile(`\[{{(\S+)}}`)
+	optClauses := make(map[string]string)
+	optClausesPattern := regexp.MustCompile(`\[{{(\S+)}}`)
 
-    if optClausesPattern.MatchString(contents) {
-        for _, matchSlice := range optClausesPattern.FindAllStringSubmatch(contents, -1) {
-            if _, exists := parameters[matchSlice[1]]; !exists {
-                optClauses[matchSlice[1]] = ""
-            } else {
-                optClauses[matchSlice[1]] = parameters[matchSlice[1]]
-            }
-        }
-    }
+	if optClausesPattern.MatchString(contents) {
+		for _, matchSlice := range optClausesPattern.FindAllStringSubmatch(contents, -1) {
+			if _, exists := parameters[matchSlice[1]]; !exists {
+				optClauses[matchSlice[1]] = ""
+			} else {
+				optClauses[matchSlice[1]] = parameters[matchSlice[1]]
+			}
+		}
+	}
 
-    return contents, optClauses
+	return contents, optClauses
 }
 
+// findTheLeaders ...
 func findTheLeaders(contents string, parameters map[string]string) (string, map[string]string, map[string]string) {
 
-    headers := make(map[string]string)
-    styles := make(map[string]string)
+	headers := make(map[string]string)
+	styles := make(map[string]string)
 
-    to_run, _, block, _ := findTheBlock(contents)
-    if !to_run {
-        return contents, headers, styles
-    }
-    _, leadersSlice := splitTheBlock(block)
+	to_run, _, block, _ := findTheBlock(contents)
+	if !to_run {
+		return contents, headers, styles
+	}
+	_, leadersSlice := splitTheBlock(block)
 
-    oldStyle := true
-    for _, l := range leadersSlice {
-        if l == "l1." {
-            oldStyle = false
-        }
-    }
+	oldStyle := true
+	for _, l := range leadersSlice {
+		if l == "l1." {
+			oldStyle = false
+		}
+	}
 
-    for _, leader := range leadersSlice {
-        if oldStyle {
-            leader = strconv.Itoa(len(leader)-1)
-            leader = "level-" + leader
-        } else {
-            leader = string(leader[1])
-            leader = "level-" + leader
-        }
-        if _, exists := parameters[leader]; !exists {
-            headers[leader] = ""
-        } else {
-            headers[leader] = parameters[leader]
-        }
-    }
+	for _, leader := range leadersSlice {
+		if oldStyle {
+			leader = strconv.Itoa(len(leader) - 1)
+			leader = "level-" + leader
+		} else {
+			leader = string(leader[1])
+			leader = "level-" + leader
+		}
+		if _, exists := parameters[leader]; !exists {
+			headers[leader] = ""
+		} else {
+			headers[leader] = parameters[leader]
+		}
+	}
 
-    stylesSlice := []string{"no-indent", "no-reset", "level-style"}
+	stylesSlice := []string{"no-indent", "no-reset", "level-style"}
 
-    for _, style := range stylesSlice {
-        styles = assembleStyle(style, parameters, styles)
-    }
+	for _, style := range stylesSlice {
+		styles = assembleStyle(style, parameters, styles)
+	}
 
-    return contents, headers, styles
+	return contents, headers, styles
 }
 
+// assembleStyle ...
 func assembleStyle(style string, parameters map[string]string, styles map[string]string) map[string]string {
-    if _, exists := parameters[style]; !exists {
-        styles[style] = ""
-    } else {
-        styles[style] = parameters[style]
-    }
-    return styles
+	if _, exists := parameters[style]; !exists {
+		styles[style] = ""
+	} else {
+		styles[style] = parameters[style]
+	}
+	return styles
 }
 
-func reAssembleTheFile(contents string, mixins map[string]string, optClauses map[string]string, headers map[string]string, styles map[string]string) (string) {
+// reAssembleTheFile ...
+func reAssembleTheFile(contents string, mixins map[string]string, optClauses map[string]string, headers map[string]string, styles map[string]string) string {
 
-    if !(len(mixins) == 0) || !(len(optClauses) == 0) || !(len(headers) == 0) {
-        frontMatter := "---\n\n"
-        if !(len(mixins) == 0) {
-            mixinsAsByteArray, _ := yaml.Marshal(mixins)
-            mixinString := string(mixinsAsByteArray)
-            frontMatter = frontMatter + "# Mixins\n" + mixinString
-        }
-        if !(len(mixins) == 0) && !(len(optClauses) == 0) {
-            frontMatter = frontMatter + "\n"
-        }
-        if !(len(optClauses) == 0) {
-            optClausesAsByteArray, _ := yaml.Marshal(optClauses)
-            optClausesString := string(optClausesAsByteArray)
-            frontMatter = frontMatter + "# Optional Clauses\n" + optClausesString
-        }
-        if ( !(len(mixins) == 0) || !(len(optClauses) == 0) ) && !(len(headers) == 0) {
-            frontMatter = frontMatter + "\n"
-        }
-        if !(len(headers) == 0) {
-            headersAsByteArray, _ := yaml.Marshal(headers)
-            headersString := string(headersAsByteArray)
-            stylesAsByteArray, _ := yaml.Marshal(styles)
-            stylesString := string(stylesAsByteArray)
-            frontMatter = frontMatter + "# Structured Headers\n" + headersString
-            frontMatter = frontMatter + "\n# Properties\n" + stylesString
-        }
-        frontMatter = frontMatter + "\n---\n\n"
-        contents = frontMatter + contents
-    }
+	if !(len(mixins) == 0) || !(len(optClauses) == 0) || !(len(headers) == 0) {
+		frontMatter := "---\n\n"
+		if !(len(mixins) == 0) {
+			mixinsAsByteArray, _ := yaml.Marshal(mixins)
+			mixinString := string(mixinsAsByteArray)
+			frontMatter = frontMatter + "# Mixins\n" + mixinString
+		}
+		if !(len(mixins) == 0) && !(len(optClauses) == 0) {
+			frontMatter = frontMatter + "\n"
+		}
+		if !(len(optClauses) == 0) {
+			optClausesAsByteArray, _ := yaml.Marshal(optClauses)
+			optClausesString := string(optClausesAsByteArray)
+			frontMatter = frontMatter + "# Optional Clauses\n" + optClausesString
+		}
+		if (!(len(mixins) == 0) || !(len(optClauses) == 0)) && !(len(headers) == 0) {
+			frontMatter = frontMatter + "\n"
+		}
+		if !(len(headers) == 0) {
+			headersAsByteArray, _ := yaml.Marshal(headers)
+			headersString := string(headersAsByteArray)
+			stylesAsByteArray, _ := yaml.Marshal(styles)
+			stylesString := string(stylesAsByteArray)
+			frontMatter = frontMatter + "# Structured Headers\n" + headersString
+			frontMatter = frontMatter + "\n# Properties\n" + stylesString
+		}
+		frontMatter = frontMatter + "\n---\n\n"
+		contents = frontMatter + contents
+	}
 
-    return contents
+	return contents
 }
